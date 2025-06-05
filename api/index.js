446d6e02dfd7ed3985da73df3e1f3a4f6f39c198
luizgustavogg <luizgustavoggdev@gmail.com>
2025-06-05 20:58:47 -0300

    fix: just export default serveless to index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+// /api/index.js
 import express from "express";
 import serverless from "serverless-http";
 
@@ -12,16 +13,4 @@ app.use("/users", userRoutes);
 app.use("/notices", noticeRoutes);
 app.use("/preferences", preferenceRoutes);
 
-const handlerRaw = serverless(app);
-
-export const handler = async (event, context) => {
-  try {
-    return await handlerRaw(event, context);
-  } catch (err) {
-    console.error("ERRO NO HANDLER:", err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Erro interno no servidor" }),
-    };
-  }
-};
+export default serverless(app);
