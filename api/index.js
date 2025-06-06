b95c35e7676ccce4a86728f12bbc80733b298ff3
luizgustavogg <luizgustavoggdev@gmail.com>
2025-06-05 22:27:27 -0300

    fix: minimal test

--- api/index.js
+++ api/index.js
@@ -1,23 +1,14 @@
-// /api/index.js
 import express from "express";
 import serverless from "serverless-http";
 import dotenv from "dotenv";
-import userRoutes from "./user.js";
-import noticeRoutes from "./notices.js";
-import preferenceRoutes from "./preferences.js";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
+// ROTA DE TESTE EXTREMAMENTE SIMPLES:
 app.get("/health", (req, res) => {
   return res.json({ status: "ok", timestamp: Date.now() });
 });
-
-// Rotas originais
-app.use("/users", userRoutes);
-app.use("/notices", noticeRoutes);
-app.use("/preferences", preferenceRoutes);
 
 export default serverless(app);
